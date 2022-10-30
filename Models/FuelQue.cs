66279128a0cbd26afe75a5d6b5b9e8c3ab3ea4e8
asqarrsl <asqarrsl@gmail.com>
2022-10-31 01:46:00 +0530

    Changes

--- Models/FuelQue.cs
+++ Models/FuelQue.cs
@@ -1,30 +1,26 @@
-﻿/*
-* FuelQue: class -  fuel queue model in database is represented
-*/
-
-using MongoDB.Bson;
+﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-
+/*
+* FuelQueue: class - Represents fuel queue model in database
+*/
 namespace equeue_server.Models
 {
-    public class FuelQue
+    public class FuelQueue
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; } = String.Empty;
 
-
+        [BsonElement("fuelStationId")]
+        public string FuelStationId { get; set; } = String.Empty;
 
         [BsonElement("numberOfVehicles")]
         public int NumberOfVehicles { get; set; } = 0;
 
-        [BsonElement("fuelStationId")]
-        public string FuelStationId { get; set; } = String.Empty;
-
         [BsonElement("customers")]
         public QueueCustomer[] Customers { get; set; } = Array.Empty<QueueCustomer>();
 
-        public FuelQue() { }
+        public FuelQueue() {}
     }
 }
